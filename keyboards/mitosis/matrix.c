7d1729ba386cbf41d595ff38d90c64c2a68c0b85
Michael F. Lamb <mike@datagrok.org>
2019-12-18 20:39:33 -0800

    mitosis: (no-op) reduce the delta between matrix.c and /quantum/matrix.c

--- keyboards/mitosis/matrix.c
+++ keyboards/mitosis/matrix.c
@@ -1,6 +1,5 @@
 /*
-Copyright 2012 Jun Wako
-Copyright 2014 Jack Humbert
+Copyright 2012-2018 Jun Wako, Jack Humbert, Yiancar
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -45,35 +44,44 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 /* matrix state(1:on, 0:off) */
-static matrix_row_t matrix[MATRIX_ROWS];
+static matrix_row_t matrix[MATRIX_ROWS];      // debounced values
 
 
-__attribute__ ((weak))
-void matrix_init_kb(void) {
-    matrix_init_user();
+__attribute__((weak)) void matrix_init_kb(void) { matrix_init_user(); }
+
+__attribute__((weak)) void matrix_scan_kb(void) { matrix_scan_user(); }
+
+__attribute__((weak)) void matrix_init_user(void) {}
+
+__attribute__((weak)) void matrix_scan_user(void) {}
+
+inline uint8_t matrix_rows(void) { return MATRIX_ROWS; }
+
+inline uint8_t matrix_cols(void) { return MATRIX_COLS; }
+
+inline bool matrix_is_on(uint8_t row, uint8_t col) { return (matrix[row] & ((matrix_row_t)1 << col)); }
+
+inline matrix_row_t matrix_get_row(uint8_t row) {
+    return matrix[row];
 }
 
-__attribute__ ((weak))
-void matrix_scan_kb(void) {
-    matrix_scan_user();
+void matrix_print(void) {
+    print_matrix_header();
+
+    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+        phex(row);
+        print(": ");
+        print_matrix_row(row);
+        print("\n");
+    }
 }
 
-__attribute__ ((weak))
-void matrix_init_user(void) {
-}
-
-__attribute__ ((weak))
-void matrix_scan_user(void) {
-}
-
-inline
-uint8_t matrix_rows(void) {
-    return MATRIX_ROWS;
-}
-
-inline
-uint8_t matrix_cols(void) {
-    return MATRIX_COLS;
+uint8_t matrix_key_count(void) {
+    uint8_t count = 0;
+    for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
+        count += matrix_bitpop(i);
+    }
+    return count;
 }
 
 void matrix_init(void) {
@@ -81,8 +89,12 @@ void matrix_init(void) {
     matrix_init_quantum();
 }
 
-uint8_t matrix_scan(void)
-{
+
+
+
+
+uint8_t matrix_scan(void) {
+
     SERIAL_UART_INIT();
 
     uint32_t timeout = 0;
@@ -120,36 +132,4 @@ uint8_t matrix_scan(void)
 
     matrix_scan_quantum();
     return 1;
-}
-
-inline
-bool matrix_is_on(uint8_t row, uint8_t col)
-{
-    return (matrix[row] & ((matrix_row_t)1<<col));
-}
-
-inline
-matrix_row_t matrix_get_row(uint8_t row)
-{
-    return matrix[row];
-}
-
-void matrix_print(void)
-{
-    print_matrix_header();
-
-    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
-        phex(row); print(": ");
-        print_matrix_row(row);
-        print("\n");
-    }
-}
-
-uint8_t matrix_key_count(void)
-{
-    uint8_t count = 0;
-    for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
-        count += matrix_bitpop(i);
-    }
-    return count;
 }
